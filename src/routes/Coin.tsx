684bc43b5b31a5d6b0912eab5498b268f24f5aa5
yerim kim <yrkim@inviz.co.kr>
2026-03-27 20:39:13 +0900

    react: crypto tracker (5.6) JSON 기반 타입 추론 및 VSCode 멀티 커서 활용 타입 선언

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,132 @@
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface RouteParams {
+    coinId: string;
+}
+interface RouteState {
+    name: string;
+}
+
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface PriceData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD: {
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        };
+    };
+}
+
+function Coin() {
+    const [loading, setLoading] = useState(true);
+    const { coinId } = useParams<RouteParams>();
+    const { state } = useLocation<RouteState>();
+    const [info, setInfo] = useState<any>({});
+    const [priceInfo, setPriceInfo] = useState<any>({});
+    useEffect(() => {
+        (async () => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+            ).json();
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+            ).json();
+            console.log(priceData);
+            setInfo(infoData);
+            setPriceInfo(priceData);
+            // setLoading(false);
+        })();
+    }, []);
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading..."}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> :
+                <span>{info.hello}</span>
+            }
+        </Container>
+    );
+}
+export default Coin;
+
+/* Type 선언 팁 */
+/* 
+    1. json 데이터를 전역변수로 선언한다.
+    2. json의 key값을 우선 가져와 key값을 만든다.
+    ex) Object.keys(temp1).join()
+    3. json의 value값을 이용해서 interface의 value 타입을 지정한다.
+    ex) Object.values(temp1).map(v => typeof v).join()
+    key값에 value 넣기
+    ctrl+shilft+L -> 여러 줄 선택 -> 붙여넣기
+*/
